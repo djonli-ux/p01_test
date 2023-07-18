14534c9e6cde0df3e1649f77699ea72e3ef91fb4
Jonatan Lifshits <djonli2003@gmail.com>
2023-07-18 15:32:53 +0300

    13. dev: fix2: main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ main() {
 	
 	std::cout << "Hello World!!!" << endl;
 
-	int a = 560; // ==== in dev 
-	int b = 34;
+	int a = 560; // ==== in dev 1
+	int b = 340; // ==== in dev 2
 	std::cout << a + b << endl;
 
 	std::string email = "vasia@gmail.com";
